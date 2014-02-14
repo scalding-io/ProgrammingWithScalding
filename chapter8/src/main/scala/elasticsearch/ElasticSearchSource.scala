c62ee1123c845d08a58329184ae70b13f0166690
Antonios Chalkiopoulos <Antwnis@gmail.com>
2014-02-14 17:01:04 +0000

    Adding initial examples of Scalding + ElasticSearch

---
+++ chapter8/src/main/scala/elasticsearch/ElasticSearchSource.scala
@@ -0,0 +1,62 @@
+package elasticsearch
+
+import org.elasticsearch.hadoop.cascading._
+import com.twitter.scalding._
+
+import cascading.tap.SinkMode
+import cascading.tap.Tap
+import cascading.tuple.Fields
+
+/**
+ * https://github.com/rore/SpyGlass/blob/master/src/main/scala/parallelai/spyglass/hbase/HBaseSource.scala
+ * https://github.com/twitter/scalding/blob/02a7affe768850cb39e959413020d52ffa3987e9/scalding-core/src/main/scala/com/twitter/scalding/FileSource.scala
+ */
+case class ElasticSearchSource(
+   es_host :String="localhost",
+   es_port :Int   = 9200,
+   es_resource:String="scalding_index/type")
+  extends Source {
+
+  val sinkMode: SinkMode = SinkMode.REPLACE
+
+  def createLocalTap(sinkMode: SinkMode): Tap[_, _, _] =
+    new EsTap(es_host, es_port, es_resource,"",Fields.ALL)
+
+  override def createTap(readOrWrite: AccessMode)(implicit mode: Mode): Tap[_, _, _] = {
+    mode match {
+      case Local(_) => {
+        createLocalTap(sinkMode)
+      }
+    }
+  }
+}
+//      case hdfsMode@Hdfs(_, _) => readOrWrite match {
+//        case Read => createHdfsReadTap(hdfsMode)
+//        //case Write => EsTap() // CastHfsTap(new Hfs(hdfsScheme, hdfsWritePath, sinkMode))
+//      }
+//      case _ => {
+//        new Tap[_,_,_] //println("aaaa")
+//      }
+
+//   val hbt = new EsTap() //"quorumNames", "tableName", "hBaseScheme", "SinkMode.KEEP"
+//   hbt.asInstanceOf[Tap[_,_,_]]
+//    }
+//
+//  }
+
+//  protected def createHdfsReadTap(hdfsMode : Hdfs) : Tap[JobConf, _, _] = {
+//    val taps : List[Tap[JobConf, RecordReader[_,_], OutputCollector[_,_]]] =
+//      goodHdfsPaths(hdfsMode).toList.map { path => CastHfsTap(new Hfs(hdfsScheme, path, sinkMode)) }
+//    taps.size match {
+//      case 0 => {
+//        // This case is going to result in an error, but we don't want to throw until
+//        // validateTaps, so we just put a dummy path to return something so the
+//        // Job constructor does not fail.
+//        CastHfsTap(new Hfs(hdfsScheme, hdfsPaths.head, sinkMode))
+//      }
+//      case 1 => taps.head
+//  //    case _ => new ScaldingMultiSourceTap(taps)
+//    }
+//  }
+
+//}
---
+++ chapter8/src/main/scala/elasticsearch/ElasticSearchUpdateIndexes.scala
@@ -0,0 +1,28 @@
+package elasticsearch
+
+import com.twitter.scalding._
+import org.elasticsearch.hadoop.cascading._
+
+// ElasticSearch example
+class ElasticSearchUpdateIndexes(args: Args) extends Job(args) {
+
+  // Some data to push into elastic-search
+  val someData = List(
+    ("product1", "description1"),
+    ("product2", "description2"),
+    ("product3", "description3"))
+
+  val elasticMapping = ( "string", "string/date")
+  val elasticId = Field("product","description")
+
+  // This example is equivalent to using `sum`, but you can also supply other reduce functions.
+  val indexNewDataInElasticSearch =
+    IterableSource[(String, String)](someData, ('product, 'description))
+      .write(elasticsearch.ElasticSearchSource("localhost", 9200,"index_es/type_es"))
+
+  val readDataFromElasticSearch =
+     ElasticSearchSource("localhost", 9200,"index_es/type_es").read
+     .write(Tsv("dummy"))
+
+
+}
