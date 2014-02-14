3e76ced352c3f3cbcc41fae2d593ecad6bd2a31a
Antonios Chalkiopoulos <Antwnis@gmail.com>
2014-02-14 17:03:52 +0000

    Quick fix - till code improves

--- chapter8/src/main/scala/elasticsearch/ElasticSearchUpdateIndexes.scala
+++ chapter8/src/main/scala/elasticsearch/ElasticSearchUpdateIndexes.scala
@@ -12,17 +12,17 @@ class ElasticSearchUpdateIndexes(args: Args) extends Job(args) {
     ("product2", "description2"),
     ("product3", "description3"))
 
-  val elasticMapping = ( "string", "string/date")
-  val elasticId = Field("product","description")
+  //val elasticMapping = ( "string", "string/date")
+  //val elasticId = Field("product","description")
 
   // This example is equivalent to using `sum`, but you can also supply other reduce functions.
   val indexNewDataInElasticSearch =
     IterableSource[(String, String)](someData, ('product, 'description))
       .write(elasticsearch.ElasticSearchSource("localhost", 9200,"index_es/type_es"))
 
-  val readDataFromElasticSearch =
-     ElasticSearchSource("localhost", 9200,"index_es/type_es").read
-     .write(Tsv("dummy"))
+//  val readDataFromElasticSearch =
+//     ElasticSearchSource("localhost", 9200,"index_es/type_es").read
+//     .write(Tsv("dummy"))
 
 
 }
