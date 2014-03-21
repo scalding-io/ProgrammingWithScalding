03e2cafa5dc1944eb5cfabed4529a0106f75e5c6
Antonios Chalkiopoulos <Antwnis@gmail.com>
2014-03-21 14:43:26 +0000

    minor changes

--- chapter8/src/main/scala/elasticsearch/ElasticSearchUpdateIndexes.scala
+++ chapter8/src/main/scala/elasticsearch/ElasticSearchUpdateIndexes.scala
@@ -2,9 +2,11 @@ package elasticsearch
 
 import com.twitter.scalding._
 import cascading.tuple.Fields
+import java.util.Properties
+import org.elasticsearch.hadoop.cfg.ConfigurationOptions
 
 // ElasticSearch example
-class ElasticSearchUpdateIndexes(args: Args) extends JobBase(args) {
+class ElasticSearchUpdateIndexes(args: Args) extends Job(args) {
 
   // Some data to push into elastic-search
   val someData = List(
@@ -12,10 +14,16 @@ class ElasticSearchUpdateIndexes(args: Args) extends JobBase(args) {
     ("2","product2", "description2"),
     ("3","product3", "description3"))
 
+  val elasticSearchProperties = new Properties()
+  elasticSearchProperties.setProperty(ConfigurationOptions.ES_WRITE_OPERATION, "index")
+  elasticSearchProperties.setProperty(ConfigurationOptions.ES_MAPPING_ID     , "number")
+
+//  elasticSearch
+
   val schema = List('number, 'product, 'description)
   val indexNewDataInElasticSearch =
     IterableSource[(String,String, String)](someData, schema)
-      .write(ElasticSearchSource("dev.weather.marine.travel", 9200,"index_es/type_es", schema))
+      .write(ElasticSearchSource("dev.weather.marine.travel", 9200,"index_es/type_es", schema, elasticSearchProperties))
 
 //  val readDataFromElasticSearch =
 //     ElasticSearchSource("localhost", 9200,"index_es/type_es").read
