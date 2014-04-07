85ed767fab16ae6edcb63a2dc122379f3f36cf4f
Antonios Chalkiopoulos <Antwnis@gmail.com>
2014-04-07 21:51:04 +0100

    A more meaningful example for foldLeft - kudos to Gracia

--- chapter3/src/main/scala/foldLeft.scala
+++ chapter3/src/main/scala/foldLeft.scala
@@ -3,30 +3,42 @@ import com.twitter.scalding._
 // foldLeft example
 class foldLeft(args: Args) extends Job(args) {
 
-  // A set of shopping carts.
-  // Each product has an original price and a discount
-  val cartList = List(
-    ("cart1", 100.0, 10),   // 90
-    ("cart1", 10.0,  10),   // 9
-    ("cart1", 10.0 , 90),   // 1
-    ("cart2", 50.0,  20),   // 40
-    ("cart2", 50.0,  10),   // 45
-    ("cart2", 25.0,  20))   // 20
+  // A set of customers. Some of them are paying for service-A
+  // some of them for service-B etc
+  val customerList = List(
+    // Customer 1 is paying for service-A AND service-B
+    ("customer1", "service-A", true),
+    ("customer1", "service-B", true),
+    // Customer 2 is paying for service-A
+    ("customer2", "service-A", true),
+    ("customer2", "service-B", false),
+    // Customer 3 is paying for service-B
+    ("customer3", "service-A", false),
+    ("customer3", "service-B", true),
+    // Customer 4 is not paying
+    ("customer4", "service-A", false),
+    ("customer4", "service-B", false))
 
-  // This example is equivalent to using `sum`, but you can also supply other reduce functions.
-  val costPerProjectPhase =
-    IterableSource[(String, Double, Int)](cartList, ('cart, 'price, 'discount))
-    .groupBy('cart) {
-      val initial_cost = 5.0
-      group => group.foldLeft(('price, 'discount)-> 'total_cost)(initial_cost) {
-        (initial_cost: Double, input: (Double, Double)) =>
-          val (price, discount) = input
-          initial_cost + price * ( 100 - discount )/100
+    // To identify paying-customers we can use a foldLeft on the group
+    // The foldLeft in Scalding is returning a single result
+  val payingCustomer =
+    IterableSource[(String, String, Boolean)](customerList, ('customer, 'service, 'is_paying))
+    .groupBy('customer) {
+      // By default a customer is NOT paying - unless there is a 'true'
+      group => group.foldLeft('is_paying -> 'is_paying)(false) {
+        (prev: Boolean, current: Boolean) =>
+          prev || current
       }
     }
     .debug
     .write(Tsv("foldLeft"))
 
-
+  /*
+   Result is:
+     customer1 true
+     customer2 true
+     customer3 true
+     customer4 false
+   */
 }
 
