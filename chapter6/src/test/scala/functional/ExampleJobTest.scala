f46413f2ad100a6d530f8cb053850a24b4094c21
Antonios Chalkiopoulos <Antwnis@gmail.com>
2014-05-26 12:36:57 +0100

    Completing TDD implementation

--- chapter6/src/test/scala/functional/ExampleJobTest.scala
+++ chapter6/src/test/scala/functional/ExampleJobTest.scala
@@ -13,23 +13,34 @@ class ExampleJobTest extends FlatSpec with Matchers with FieldConversions with T
 
   import ExampleSchema._
 
-  "FUNCTIONAL - A sample job" should "do the full transformation" in {
+  "FUNCTIONAL2 TEST - A sample job" should "do the full transformation" in {
+
+    val logs = List(
+      ("01/07/2014 10:22:11", 1000002L, "http://youtube.com"),
+      ("01/07/2014 10:22:11", 1000003L, "http://twitter.com"),
+      ("01/07/2014 10:22:11", 1000002L, "http://google.com"),
+      ("01/07/2014 10:22:11", 1000002L, "http://facebook.com")
+    )
+    val users = List(
+      (1000002L, "stefano@email.com", "10 Downing St. London"),
+      (1000003L, "antonios@email.com", "1 Kingdom St. London")
+    )
+    val expectedOutput = List(
+      ("2014/07/01", 1000002L, 3L, "stefano@email.com","10 Downing St. London"),
+      ("2014/07/01", 1000003L, 1L, "antonios@email.com", "1 Kingdom St. London")
+    )
 
     JobTest(classOf[ExampleJob].getName)
       .arg("input", "input-logs")
       .arg("users", "users-logs")
       .arg("output1", "output-logs-daily-visits")
       .arg("output2", "output-logs-daily-with-address")
-      .source(Tsv("input-logs", LOG_SCHEMA), List(("11/02/2013 10:22:11", 1000002l, "http://www.youtube.com")))
-      .source(Tsv("users-logs", USER_SCHEMA), List((1000002l, "stefano@email.com", "10 Downing St. London")))
-      .sink(Tsv("output-logs-daily-visits", LOGS_DAILY_VISITS)) {
-        buffer: mutable.Buffer[(String, Long, Long)] =>
-          buffer.toList shouldEqual List(("2013/02/11", 1000002l, 1l))
-      }
+      .source(Tsv("input-logs", LOG_SCHEMA), logs)
+      .source(Tsv("users-logs", USER_SCHEMA), users)
       .sink(Tsv("output-logs-daily-with-address", LOG_DAILY_WITH_ADDRESS)) {
-      buffer: mutable.Buffer[(String, Long, Long, String, String)] =>
-          buffer.toList shouldEqual List(("2013/02/11", 1000002l, 1l, "stefano@email.com", "10 Downing St. London"))
+        buffer: mutable.Buffer[(String, Long, Long, String, String)] =>
+          buffer.toList should equal(expectedOutput)
       }
-      .run
+      .run // or .runHadoop
   }
 }
