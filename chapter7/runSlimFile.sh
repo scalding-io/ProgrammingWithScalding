5d3fa4dfc71e1bf2263b101986b96c6244d6b540
Antonios Chalkiopoulos <Antwnis@gmail.com>
2014-06-16 17:17:32 +0100

    Completing chapter 7 examples

--- chapter7/runSlimFile.sh
+++ chapter7/runSlimFile.sh
@@ -7,14 +7,14 @@ export DRIVEN_API_KEY=D991A15E7A174E098900CDEE4F3A3CA6
 MAIN_JAR=target/chapter7-0.jar
 
 # Aggregate all dependencies into the HADOOP_CLASSPATH
-for f in alternateLocation/*.jar; do CP=$f:$CP; done
+for f in libs/*.jar; do CP=$f:$CP; done
 export HADOOP_CLASSPATH=$MAIN_JAR:$CP
 
 # The following block does not need to be executed every time
 # as our dependencies don't change frequently 
 hadoop fs -mkdir -p project1/libs/
 hadoop fs -rm -skipTrash project1/libs/*
-hadoop fs -put alternateLocation/* project1/libs/
+hadoop fs -put libs/* project1/libs/
 hadoop fs -put $MAIN_JAR project1/libs/
 
 # Runs : a Scalding Job | A Scala application | and then another Scalding Job
